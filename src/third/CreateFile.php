5168e19563933a3c0019ba2bb49c37696c4cf394
lzfcoder <lzfcoder@gmail.com>
2018-11-20 02:41:55 +0000

    add third

---
+++ src/third/CreateFile.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * description:
+ * author: luzhengfei <luzhengfei@smartisan.com>
+ */
+
+namespace lzfcommontool\third;
+
+class CreateFile
+{
+    public static function mmc()
+    {
+        echo '这是子类的方法';
+    }
+}
